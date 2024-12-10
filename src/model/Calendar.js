246aca74eb02d869e32421f6a4e8ae0440fc2867
jin123457 <jin_ox@naver.com>
2024-12-11 03:33:11 +0900

    Feat : 달력 모델 생성
    생성된 파일명 : Calendar.js
    Calendar.js : 달력에 있는 날짜 중 주말과 별이 있는 날짜를 생성하여 반환하는 모델을 생성

---
+++ src/model/Calendar.js
@@ -0,0 +1,19 @@
+class Calendar {
+  #weekend;
+  #specialDay;
+
+  constructor() {
+    this.#weekend = [1, 2, 8, 9, 15, 16, 22, 23, 29, 30];
+    this.#specialDay = [3, 10, 17, 24, 25, 31];
+  }
+
+  getWeekend() {
+    return this.#weekend;
+  }
+
+  getSpecialDay() {
+    return this.#specialDay;
+  }
+}
+
+export default Calendar;
