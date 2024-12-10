c722e97a5bb8b205b40ec0ed2f76c1c6e80bb159
jin123457 <jin_ox@naver.com>
2024-12-11 03:59:59 +0900

    Refactor : 메뉴 모델 형식에 맞게 수정
    수정된 파일명 : Calendar.js
    Calendar.js : 메뉴 모델과 동일하게 create함수에서 해당 내용을 넣도록 수정

--- src/model/Calendar.js
+++ src/model/Calendar.js
@@ -3,6 +3,11 @@ class Calendar {
   #specialDay;
 
   constructor() {
+    this.#weekend = [];
+    this.#specialDay = [];
+  }
+
+  async create() {
     this.#weekend = [1, 2, 8, 9, 15, 16, 22, 23, 29, 30];
     this.#specialDay = [3, 10, 17, 24, 25, 31];
   }
