af64ebcf767cdb0e5b9ac1e44d10e82e010d25df
jin123457 <jin_ox@naver.com>
2024-12-11 04:02:47 +0900

    Feat : 메뉴 모델 생성
    생성된 파일명 : Menu.js
    Menu.js : 각 메뉴에 대한 정보를 저장하고 getter로 메뉴의 정보를 호출할 수 있는 생성
    
    수정된 파일명 : App.js
    App.js : 해당 파일에서 모델들을 새로 생성하고 호출

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import Calendar from './model/Calendar.js';
+import Menu from './model/Menu.js';
+
 class App {
-  async run() {}
+  async run() {
+    const newCalendar = new Calendar();
+    const newMenu = new Menu();
+    await newCalendar.create();
+    await newMenu.create();
+  }
 }
 
 export default App;
---
+++ src/model/Menu.js
@@ -0,0 +1,28 @@
+class Menu {
+  #menuList;
+  
+  constructor() {
+    this.#menuList = new Map();
+  }
+
+  async create() {
+    this.#menuList
+      .set('양송이수프', ['appetizer', 6000])
+      .set('타파스', ['appetizer', 5500])
+      .set('시저샐러드', ['appetizer', 8000])
+      .set('티본스테이크', ['main', 55000])
+      .set('바비큐립', ['main', 54000])
+      .set('해산물파스타', ['main', 35000])
+      .set('초코케이크', ['desert', 15000])
+      .set('아이스크림', ['desert', 5000])
+      .set('제로콜라', ['drink', 3000])
+      .set('레드와인', ['drink', 60000])
+      .set('샴페인', ['drink', 25000]);
+  }
+
+  getFoodInfo(name) {
+    return this.#menuList.get(name);
+  }
+}
+
+export default Menu;
